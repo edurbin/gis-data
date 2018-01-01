8f58a1964865f8c7ec5f2680689b2c808c6fd6e3
Eric Durbin <eadurbin@gmail.com>
2017-12-31 23:30:34 -0600

    clean up formatting

--- js/fetch_rwis_observations.js
+++ js/fetch_rwis_observations.js
@@ -10,14 +10,14 @@ var url_array = [rwis_surface_url, rwis_atmos_url];
 var rwisObservations = {};
 var atmosObservations = {}; //only 1 atmospheric observation per site
 var siteMap = {};
-function isSurfaceData(row){
+function isSurfaceData(row) {
     return row.length == 15 ? true : false;
 }
 
-var buildAtmosphericObservation = function(row) {
+var buildAtmosphericObservation = function (row) {
     var rpuId = row[1];
     var timestamp = row[3];
-    var obsKey = JSON.stringify({rpu_id: rpuId, time: timestamp});
+    var obsKey = JSON.stringify({ rpu_id: rpuId, time: timestamp });
     //TODO clean up values, convert to Farenheit, average wind direction min/max
     atmosObservations[obsKey] = {
         air_temp: row[4],
@@ -32,14 +32,14 @@ var buildAtmosphericObservation = function(row) {
         precipitation_rate: row[14],
         accumulation: row[15],
         visibility: row[16]
-    };    
+    };
 }
 
-var buildSurfaceObservations = function(row) {
+var buildSurfaceObservations = function (row) {
     var rpuId = row[1];
     var sensorId = row[2];
     var timestamp = row[3];
-    var obsKey = JSON.stringify({rpu_id: rpuId, sensor_id: sensorId, time: timestamp});
+    var obsKey = JSON.stringify({ rpu_id: rpuId, sensor_id: sensorId, time: timestamp });
     var surfaceCondition = row[4];
     var surfaceTemp = row[5];
     var freezeTemp = row[6];
@@ -53,52 +53,52 @@ var buildSurfaceObservations = function(row) {
         depth: depth,
         ice_pct: icePct,
         sub_temp: subTemp
-    }); 
+    });
 }
 
 
-RwisSite.find({}, function(err, rwisSites){
-rwisSites.forEach(function(rwisSite){
-    var rpuId = rwisSite.rpu_id;
-    var sensorId = rwisSite.sensor_id;
-    var siteKey = JSON.stringify({rpu_id: rpuId, sensor_id: sensorId});
-    siteMap[siteKey] = rwisSite;        
-});
-
-var promises = url_array.map(url => fetch(url).then(response => response.text()));
-Promise.all(promises).then(results => {
-    results.forEach(function(data){
-        var rows = CSV.parse(data);
-        var firstRow = rows.shift();
-        rows.forEach(function(row){
-	    if(isSurfaceData(firstRow)){
-	        buildSurfaceObservations(row);
-            } else {
-                buildAtmosphericObservation(row);
-            }
-        });
+RwisSite.find({}, function (err, rwisSites) {
+    rwisSites.forEach(function (rwisSite) {
+        var rpuId = rwisSite.rpu_id;
+        var sensorId = rwisSite.sensor_id;
+        var siteKey = JSON.stringify({ rpu_id: rpuId, sensor_id: sensorId });
+        siteMap[siteKey] = rwisSite;
     });
-        var actions = Object.keys(rwisObservations).map(function(key, index){
+
+    var promises = url_array.map(url => fetch(url).then(response => response.text()));
+    Promise.all(promises).then(results => {
+        results.forEach(function (data) {
+            var rows = CSV.parse(data);
+            var firstRow = rows.shift();
+            rows.forEach(function (row) {
+                if (isSurfaceData(firstRow)) {
+                    buildSurfaceObservations(row);
+                } else {
+                    buildAtmosphericObservation(row);
+                }
+            });
+        });
+        var actions = Object.keys(rwisObservations).map(function (key, index) {
             var rwisObservation = rwisObservations[key];
-            var keyObj  = JSON.parse(key);
+            var keyObj = JSON.parse(key);
             var rpuId = keyObj.rpu_id;
             var sensorId = keyObj.sensor_id;
             var timestamp = keyObj.time;
-            var obsKey = JSON.stringify({rpu_id: rpuId, time: timestamp});
-            if(atmosObservations[obsKey]) {
+            var obsKey = JSON.stringify({ rpu_id: rpuId, time: timestamp });
+            if (atmosObservations[obsKey]) {
                 var atmosphericObservation = atmosObservations[obsKey];
-                Object.keys(atmosphericObservation).forEach(function(atmosKey){
+                Object.keys(atmosphericObservation).forEach(function (atmosKey) {
                     rwisObservation[atmosKey] = atmosphericObservation[atmosKey];
                 });
             }
-            var siteKey = JSON.stringify({rpu_id: rpuId, sensor_id: sensorId});
+            var siteKey = JSON.stringify({ rpu_id: rpuId, sensor_id: sensorId });
             var rwisSite = siteMap[siteKey];
             rwisObservation.rwis_site = rwisSite;
             var promise = rwisObservation.save();
             return promise;
-        
-        }); 
-        Promise.all(actions).then(function(){
+
+        });
+        Promise.all(actions).then(function () {
             mongoose.connection.close();
         });
     });
--- js/load_rwis_sites.js
+++ js/load_rwis_sites.js
@@ -6,45 +6,45 @@ var RwisSite = require('./models/rwis_sites');
 var CSV = require('csv-string');
 var db = mongoose.connection;
 
-var fn = function buildRwisSite(row){
-        var rpuId = row[0];
-        var sensorName = row[1];
-        var township = row[2];
-        var lat = row[7];
-        var lon = row[8];
-        var altitude = row[9];
-        var county = row[10];
-        var routeName = row[11];
-        var milePost = row[12];
-        var garageName = row[14];
-        var sensorId = row[17];
-        var priority = row[19];
+var fn = function buildRwisSite(row) {
+    var rpuId = row[0];
+    var sensorName = row[1];
+    var township = row[2];
+    var lat = row[7];
+    var lon = row[8];
+    var altitude = row[9];
+    var county = row[10];
+    var routeName = row[11];
+    var milePost = row[12];
+    var garageName = row[14];
+    var sensorId = row[17];
+    var priority = row[19];
 
-	var rwisSite = new RwisSite({
-            rpu_id: rpuId,
-            county_name: county,
-            mile_post: milePost,
-            route_name: routeName,
-            sensor_id: sensorId,
-            sensor_name: sensorName,
-            garage: garageName,
-            altitude: altitude,
-            township: township,
-            priority: priority,
-            lat: lat,
-            lon: lon 
-        });
-        var promise = rwisSite.save();
-        return promise;
+    var rwisSite = new RwisSite({
+        rpu_id: rpuId,
+        county_name: county,
+        mile_post: milePost,
+        route_name: routeName,
+        sensor_id: sensorId,
+        sensor_name: sensorName,
+        garage: garageName,
+        altitude: altitude,
+        township: township,
+        priority: priority,
+        lat: lat,
+        lon: lon
+    });
+    var promise = rwisSite.save();
+    return promise;
 }
 
-fs.readFile(rwis_sites, 'utf8', function(err,data){
-    if(err) {
+fs.readFile(rwis_sites, 'utf8', function (err, data) {
+    if (err) {
         return console.log(err);
     }
     var csvArr = CSV.parse(data);
     var actions = csvArr.map(fn);
-    Promise.all(actions).then(function(){
+    Promise.all(actions).then(function () {
         mongoose.connection.close();
     });
 });
