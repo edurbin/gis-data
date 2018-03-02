a2d607ad77831f10bb9283a3875c6e43649b0352
Eric Durbin <eadurbin@gmail.com>
2018-03-01 21:54:06 -0600

    remove old files

--- js/utils/visibility.js
+++
@@ -1,9 +0,0 @@
-var Visibility = function () {};
-
-Visibility.prototype.convertStatuteMiles = function(rawValue) {
-    var visibility = rawValue.replace("SM", "");    
-    visibility = eval(visibility);
-    return visibility;
-}
-
-module.exports = new Visibility();
--- js/utils/wind.js
+++
@@ -1,9 +0,0 @@
-var Wind = function () {};
-
-Wind.prototype.convertKnotsToMiles = function(rawValue) {
-    var feet = rawValue * 6076.12;
-    var miles = feet / 5280;
-    return miles.toFixed(2);
-}
-
-module.exports = new Wind();
